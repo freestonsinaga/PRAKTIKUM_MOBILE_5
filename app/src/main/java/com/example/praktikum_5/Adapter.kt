7aa0bc1aacc9927b38e35440185e6c37d18ee91c
freestonsinaga <freestonsinaga@gmail.com>
2020-11-02 16:12:44 +0700

    tugaspraktikum_5

---
+++ app/src/main/java/com/example/praktikum_5/Adapter.kt
@@ -0,0 +1,31 @@
+package com.example.praktikum_5
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.view.ViewParent
+import android.widget.Toast
+import androidx.recyclerview.widget.RecyclerView
+import java.text.FieldPosition
+import kotlinx.android.synthetic.main.list.view.*
+
+class Adapter (private val list : ArrayList<Users>) : RecyclerView.Adapter<Adapter.Holder>() {
+
+    override fun onCreateViewHolder (parent: ViewGroup, viewType: Int): Holder {
+        return  Holder(LayoutInflater.from(parent.context).inflate(R.layout.list,parent, false))
+    }
+
+    override fun getItemCount(): Int = list.size
+
+    override fun onBindViewHolder (holder: Holder, position: Int){
+        holder.view.lbList.text = list.get(position).name
+
+        holder.view.setOnClickListener{
+            Toast.makeText(holder.view.context, "Hallo" + list.get(position).name, Toast.LENGTH_SHORT).show()
+        }
+
+
+    }
+
+    class Holder(val view: View) : RecyclerView.ViewHolder(view)
+}
